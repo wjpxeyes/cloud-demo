4cad28979ce9d34d5e16b60886a0ca6873074013
xeyes <wjp508301629@163.com>
2023-09-15 16:18:55 +0800

    1.1 使用Gateway进行了网络路由，学习了gateway的断言、过滤器、跨域

---
+++ cloud-gateway/src/main/java/com/wjp/gateway/CloudGateway.java
@@ -0,0 +1,12 @@
+package com.wjp.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGateway.class, args);
+    }
+}
